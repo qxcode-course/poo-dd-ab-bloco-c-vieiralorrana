15dab60c95763c1cf57ad06deaef6d88ee9949d2
Lorrana <lorranav905@gmail.com>
2025-11-11 14:11:39 -0300

    atividade budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -1,69 +1,80 @@
 class Cliente:
     def __init__(self, nome: str):
-        self.__nome: str = nome
+        self.__nome = nome
     
     def getNome(self):
         return self.__nome
     
     def __str__(self):
         return f"{self.getNome()}"
-    
+
 class Mercantil:
-    def __init__(self, quantidade_caixa: int):
-        self.caixa: list[Cliente | None] = []
-        for _ in range(quantidade_caixa):
-            self.caixa.append(None)
+    def __init__(self, quantidade_caixas: int):
+        self.caixas: list[Cliente | None] = []
+        self.quantidade_caixas = quantidade_caixas
+        for _ in range(quantidade_caixas):
+            self.caixas.append(None)
         self.fila: list[Cliente] = []
-    
-    def __str__(self) -> str:
-        pessoas = ", ".join([("-----" if x is None else str(x)) for x in self.caixa])  
-        saida: str = f"Caixas: [{pessoas} ]\n" 
-        esperando = ", ".join([str(x) for x in self.fila])
-        saida += f"Espera: [{esperando}]" 
-        return saida
-    
+
     def arrive(self, cliente: Cliente):
         self.fila.append(cliente)
-
-    def leave(self, index: int) -> Cliente | None:
-        if index < 0 or index > len(self.caixa):
-            print("Indice invalido")
-            return
-        if self.caixa[index] is None:
-            print("Caixa vazio")
-            return
-        aux = self.caixa[index]
-        self.caixa[index] = None 
-        return aux
-    
-    def give_up(self, nome: str):
-        for i, cliente in enumerate(self.fila):
-            if cliente.getNome == nome:
-                del self.fila[i]
-                break
     
     def call(self, index: int):
-        if index < 0 or index > len(self.caixa):
-            print("Indice invalido")
+        if self.caixas[index] is not None:
+            print("fail: caixa ocupado")
             return
         if len(self.fila) == 0:
-            print("Fila vazia")
+            print("fail: sem clientes")
             return
-        if self.caixa[index] is not None:
-            print("Caixa ocupado")
-            return
-        self.caixa[index] = self.fila[0]
+        self.caixas[index] = self.fila[0]
         del self.fila[0]
+    
+    def finish(self, index: int):
+        if index < 0 or index > self.quantidade_caixas-1:
+            print("fail: caixa inexistente")
+            return
+        
+        if self.caixas[index] is None:
+            print("fail: caixa vazio")
+            return
+        
+        aux = self.caixas[index]
+        self.caixas[index] = None
+        return aux
+    
+    def __str__(self):
+        caixas = ", ".join(["-----" if x is None else str(x) for x in self.caixas])
+        saida: str = f"Caixas: [{caixas}]\n"
+        fila = ", ".join([str(x) for x in self.fila])
+        saida += f"Espera: [{fila}]"
+        return saida
+    
+def main():
+    mercantil = Mercantil(0)
+    cliente = Cliente("")
 
-mercantil = Mercantil(5)
-mercantil.arrive(Cliente("Fulana"))
-mercantil.arrive(Cliente("Ciclana"))
-mercantil.arrive(Cliente("Beltrana"))
-mercantil.call(3)
-mercantil.call(0)
-mercantil.arrive(Cliente("Louca da Silva"))
-mercantil.arrive(Cliente("Elize Matsunaga"))
-mercantil.leave(3)
-mercantil.call(1)
-mercantil.leave(0)
-print(mercantil)
+    while True:
+        line = input()
+        print("$" + line)
+        args = line.split(" ")
+
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(mercantil)
+        elif args[0] == "init":
+            quantidade_caixas = int(args[1])
+            mercantil = Mercantil(quantidade_caixas)
+        elif args[0] == "arrive":
+            nome = args[1]
+            cliente = Cliente(nome)
+            mercantil.arrive(cliente)
+        elif args[0] == "call":
+            index = int(args[1])
+            mercantil.call(index)
+        elif args[0] == "finish":
+            index = int(args[1])
+            mercantil.finish(index)
+
+
+main()
